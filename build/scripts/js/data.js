2a3c88df575cc90ffa4b98c4caaacdefd28110cc
Manuel Florez <manuelflorezw@gmail.com>
2021-10-17 20:59:24 -0500

    test data

---
+++ build/scripts/js/data.js
@@ -0,0 +1,16 @@
+const titles = [
+  {id:"title", text: 'GH Pages website'},
+  {id:"title1", text: 'Ingeniero de Sistemas'},
+  {id:"title2", text: 'Manuel Florez'}
+]
+
+const socialNetworks = [
+  {name: 'Facebook'},
+  {name: 'Youtube'},
+  {name: 'Instagram'},
+  {name: 'Twitch'},
+  {name: 'Telegram'},
+  {name: 'GitHub'}
+]
+
+export { titles, socialNetworks }
--- build/scripts/js/main.js
+++ build/scripts/js/main.js
@@ -1,1 +1,9 @@
-alert("Hola Mundo!")
+import { titles, socialNetworks } from './data.js'
+
+const insert = title => document.getElementById(title.id).innerHTML = title.text
+
+titles.forEach( insert )
+
+const pattern = (value, index) => `<li id="${index}">${value.name}</li>`
+
+insert({ id:'socialNetworks', text: socialNetworks.map( pattern ).join('') })
